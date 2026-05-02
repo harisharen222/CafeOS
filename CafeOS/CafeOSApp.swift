56ef5bd3e8d0a2998e1fc431b77925daf6b986a5
Hari Sharen <harisharen88@gmail.com>
2026-05-02 22:08:59 +0530

    Initial Commit

---
+++ CafeOS/CafeOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeOSApp.swift
+//  CafeOS
+//
+//  Created by user@1 on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CafeOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CafeOSTests/CafeOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CafeOSTests.swift
+//  CafeOSTests
+//
+//  Created by user@1 on 02/05/26.
+//
+
+import Testing
+@testable import CafeOS
+
+struct CafeOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
